90298a31312ebbbf02b7110eaf57aadcc54ce600
Stefan <hoermann@cpg-gilching.de>
2022-02-23 15:31:47 +0100

    Meeting 23.02
    LevelTemplates abgeschlossen, Levels erzeugt, GameState hinzugefuegt

--- Sokoban/Template/LevelTemplate.gd
+++ Sokoban/Template/LevelTemplate.gd
@@ -5,3 +5,10 @@ onready var Player = $Player
 func _ready():
 	for crate in $Crates.get_child_count():
 		$Crates.get_child(crate).connect("blocked", Player, "blocked")
+
+
+
+
+func _on_Goals_victory():
+	GameState.current_level += 1
+	GameState.load_level()
